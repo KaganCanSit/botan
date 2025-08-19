436f0522f44fe752ec4bee9b8b70d0f695b84949
Kağan Can Şit <kagancansit@hotmail.com>
2025-08-19 21:52:11 +0300

    Replace fuzzer macros with std::source_location

--- src/fuzzer/fuzzers.h
+++ src/fuzzer/fuzzers.h
@@ -12,6 +12,7 @@
 #include <botan/internal/target_info.h>
 #include <fstream>
 #include <iostream>
+#include <source_location>
 #include <stdint.h>
 #include <stdlib.h>  // for setenv
 #include <vector>
@@ -63,35 +64,45 @@ inline Botan::RandomNumberGenerator& fuzzer_rng() {
    return *fuzzer_rng_as_shared();
 }
 
-// TODO use a constexpr function with std::source_location
-// NOLINTNEXTLINE(*-macro-usage)
-#define FUZZER_WRITE_AND_CRASH(expr)                                                                          \
-   /* NOLINTNEXTLINE(*-avoid-do-while) */                                                                     \
-   do {                                                                                                       \
-      std::cerr << expr << " @ Line " << __LINE__ << " in " << __FILE__ << "\n"; /* NOLINT(*-macro-paren*) */ \
-      abort();                                                                                                \
-   } while(0)
+inline void FUZZER_WRITE_AND_CRASH(const std::string_view expr_str,
+                                   const std::source_location& loc = std::source_location::current()) noexcept {
+   std::cerr << expr_str << " @ Line " << loc.line() << " in " << loc.file_name() << "\n";
+   std::cerr.flush();
+   std::abort();
+}
 
-// TODO use a constexpr function with std::source_location
-// NOLINTNEXTLINE(*-macro-usage)
-#define FUZZER_ASSERT_EQUAL(x, y)                                                            \
-   /* NOLINTNEXTLINE(*-avoid-do-while) */                                                    \
-   do {                                                                                      \
-      if((x) != (y)) {                                                                       \
-         FUZZER_WRITE_AND_CRASH(#x << " = " << (x) << " != " << #y << " = " << (y) << "\n"); \
-      }                                                                                      \
-   } while(0)
+// TODO: Macro require until C++ gains reflection capabilities (possibly C++26)
+template <typename T, typename Y, std::size_t x_str_size, std::size_t y_str_size>
+inline void fuzzer_assert_equal_impl(const T& x,
+                                     const Y& y,
+                                     const char (&x_str)[x_str_size],  // Compile-time size, zero strlen
+                                     const char (&y_str)[y_str_size],  // Compile-time size, zero strlen
+                                     const std::source_location& loc = std::source_location::current()) noexcept {
+   if(x != y) {
+      std::cerr << std::string_view{x_str, x_str_size - 1} << " = " << x
+                << " != " << std::string_view{y_str, y_str_size - 1} << " = " << y << " @ Line " << loc.line() << " in "
+                << loc.file_name() << "\n";
+      std::cerr.flush();
+      std::abort();
+   }
+}
 
-// TODO use a constexpr function with std::source_location
-// NOLINTNEXTLINE(*-macro-usage)
-#define FUZZER_ASSERT_TRUE(e)                                         \
-   /* NOLINTNEXTLINE(*-avoid-do-while) */                             \
-   do {                                                               \
-      /* NOLINTNEXTLINE(*-simplify-boolean-expr) */                   \
-      if(!(e)) {                                                      \
-         FUZZER_WRITE_AND_CRASH("Expression " << #e << " was false"); \
-      }                                                               \
-   } while(0)
+#define FUZZER_ASSERT_EQUAL(x, y) fuzzer_assert_equal_impl(x, y, #x, #y, std::source_location::current());
+
+// TODO: Macro require until C++ gains reflection capabilities (possibly C++26)
+template <typename T, std::size_t expr_str_size>
+inline void fuzzer_assert_true_impl(const T& condition,
+                                    const char(&expr_str){expr_str_size},  // Compile-time size, zero strlen
+                                    const std::source_location& loc = std::source_location::current()) noexcept {
+   if(!condition) {
+      std::cerr << "Expression " << std::string_view{expr_str, expr_str_size - 1} << " was false @ Line " << loc.line()
+                << " in " << loc.file_name() << "\n";
+      std::cerr.flush();
+      std::abort();
+   }
+}
+
+#define FUZZER_ASSERT_TRUE(e) fuzzer_assert_true_impl(e, #e, std::source_location::current());
 
 #if defined(BOTAN_FUZZER_IS_TEST)
 
